042e71bf42d2bc2d6653aa2286c16f348145f9a0
Yuliia <yuliadzyoba@gmail.com>
2024-11-24 13:16:48 +0200

    add faq.js import

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -43,42 +43,39 @@ navMobLinks.forEach(link => {
   });
 });
 
-<<<<<<< HEAD
-// // ================FAQ section
-=======
 // ================FAQ section
->>>>>>> 37d70a29ee79f60d81312cd8f447b6af95e3e8c3
-// document.addEventListener('DOMContentLoaded', () => {
-//   const accordionButtons = document.querySelectorAll('.faq-question-text');
 
-//   accordionButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//       const item = button.closest('.faq-item');
-//       const answer = item.querySelector('.faq-answer');
-//       const isOpen = button.getAttribute('aria-expanded') === 'true';
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionButtons = document.querySelectorAll('.faq-question-text');
 
-//       console.log(document.querySelectorAll('.faq-answer'));
+  accordionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const item = button.closest('.faq-item');
+      const answer = item.querySelector('.faq-answer');
+      const isOpen = button.getAttribute('aria-expanded') === 'true';
 
-//       document.querySelectorAll('.faq-item').forEach(i => {
-//         if (i !== item) {
-//           i.querySelector('.faq-answer').style.maxHeight = null;
-//           i.classList.remove('open');
-//           i.querySelector('.faq-question-text').setAttribute(
-//             'aria-expanded',
-//             'false'
-//           );
-//         }
-//       });
+      console.log(document.querySelectorAll('.faq-answer'));
 
-//       if (isOpen) {
-//         answer.style.maxHeight = null;
-//         button.setAttribute('aria-expanded', 'false');
-//         item.classList.remove('open');
-//       } else {
-//         answer.style.maxHeight = answer.scrollHeight + 'px';
-//         button.setAttribute('aria-expanded', 'true');
-//         item.classList.add('open');
-//       }
-//     });
-//   });
-// });
+      document.querySelectorAll('.faq-item').forEach(i => {
+        if (i !== item) {
+          i.querySelector('.faq-answer').style.maxHeight = null;
+          i.classList.remove('open');
+          i.querySelector('.faq-question-text').setAttribute(
+            'aria-expanded',
+            'false'
+          );
+        }
+      });
+
+      if (isOpen) {
+        answer.style.maxHeight = null;
+        button.setAttribute('aria-expanded', 'false');
+        item.classList.remove('open');
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + 'px';
+        button.setAttribute('aria-expanded', 'true');
+        item.classList.add('open');
+      }
+    });
+  });
+});
