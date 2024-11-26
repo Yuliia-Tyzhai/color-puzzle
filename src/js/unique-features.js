6a52231075988dce28c3fa41cfc4876bce4bd5c2
Yuliia <yuliadzyoba@gmail.com>
2024-11-26 04:18:02 +0200

    add fixing features

--- src/js/unique-features.js
+++ src/js/unique-features.js
@@ -20,19 +20,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const style = document.createElement('style');
     style.innerHTML = `
       .features-swiper-pagination {
-      display: flex;
-      align-items: flex-start;
-      justify-content: flex-start;
-      flex-direction: row;
-      gap: 8px;
-      margin-top: 56px;
-      margin-bottom: 53px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+        gap: 8px;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        position: relative;
       }
 
       .swiper-pagination-bullet {
         display: flex;
         align-items: center;
-        justify-content: center;  
+        justify-content: center;
         width: 40px;
         height: 40px;
         font-weight: 500;
@@ -40,16 +41,30 @@ document.addEventListener('DOMContentLoaded', function () {
         line-height: 1.6;
         color: #020202;
         padding: 10px;
-        width: 40px;
-        height: 40px;
-        background-color: #ffffff;
         opacity: 1;
         border-radius: 0px;
+        background:transparent;
       }
 
       .swiper-pagination-bullet-active {
         border: 1px solid #020202;
-        background: #ffffff;
+        background-color: #ffffff;
+      }
+
+      @media screen and (min-width: 1200px) {
+       .features-swiper-pagination { 
+       flex-direction: column; 
+       position: absolute; 
+       top: 50%;
+       left: 0; 
+       transform: translateX(-50%);
+       margin: 120px 89px 230px 40px;
+      }
+   
+        .swiper-pagination-bullet {
+          width: 80px;
+          height: 80px;
+        }
       }
     `;
     document.head.appendChild(style);
